8ad5132819a779880f19c954eb9c86027ea401ce
Javier Cobos del Alamo <jcobos-d@student.42madrid.com>
2022-05-02 21:11:45 +0200

    passes tripouille tests

--- get_next_line.h
+++ get_next_line.h
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcobos-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jcobos-d <jcobos-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 15:48:45 by jcobos-d          #+#    #+#             */
-/*   Updated: 2022/03/16 15:52:03 by jcobos-d         ###   ########.fr       */
+/*   Updated: 2022/05/02 16:28:26 by jcobos-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
 
 #endif
---
+++ main_test.c
@@ -0,0 +1,16 @@
+#include "get_next_line.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd = open("multiple_nlx5", O_RDONLY);
+	char *nextline = get_next_line(fd);
+	printf("%s \n", nextline);
+	printf("that was the first line \n");
+	char *nextline2 = get_next_line(fd);
+	printf("%s \n", nextline2);
+	printf("that was the 2nd line \n");
+	char *nextline3 = get_next_line(fd);
+	printf("%s \n", nextline3);
+}
